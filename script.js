76d75662427665621d5ec00d4793094ad11a2bb7
Angela Schumacher <angelaschumacher@web.de>
2025-01-31 20:35:19 +0100

    successfully donegit add .git add .

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const input = document.getElementById("user-input");
 
 const checkUserInput = () => {
     if(!input.value) {
-        resultsDiv.innerText = "Please provide a phone number";
+        alert("Please provide a phone number");
         return;
     }
     const regexSymbolsOrLetters = /[A-Za-z`!@#$%^&*_+=\[\]{};':"\\|,.<>\/?~]/g;
@@ -18,14 +18,36 @@ const checkUserInput = () => {
         return;
     }
 
-    const onlyNumbers = input.value.replace((/\s+|\(|\)|\-/g), "");
-    
+    let onlyNumbers = input.value.replace((/\s+|\(|\)|\-/g), "");
+
     if(onlyNumbers.length === 11 && !(/^1/).test(onlyNumbers)) {
         resultsDiv.innerText = `Invalid US number: ${input.value}`;
         return;
-    } 
+    } else if (onlyNumbers.length === 11 && (/^1/).test(onlyNumbers)) {
+        onlyNumbers = onlyNumbers.replace((/^1/), "");
+    }
    
+    const onlyNumbersAndBrackets = input.value.replace((/^1?|\s+|\-/g), "");
+    if((/\(|\)/g).test(onlyNumbersAndBrackets) && !(/^\(...\)/g).test(onlyNumbersAndBrackets)) {
+        resultsDiv.innerText = `Invalid US number: ${input.value}`;
+        return;
+    } 
+
+    const onlyNumbersAndDashes = input.value.replace((/^(1|\()?|\s+|\(|\)/g), "");
+    if((/\-/g).test(onlyNumbersAndDashes) && !(/\-[0-9]{4}$/gm).test(onlyNumbersAndDashes)) {
+        resultsDiv.innerText = `Invalid US number: ${input.value}`;
+        return;
+    } 
     
+    
+    if(onlyNumbers.length !== 10) {
+        resultsDiv.innerText = `Invalid US number: ${input.value}`;
+        return;
+    } else {
+        resultsDiv.innerText = `Valid US number:  ${input.value}`;
+        return;
+    }
+        
     
 
     
